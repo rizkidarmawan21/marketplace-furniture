5c81b076285662c8d93a971ed8af426977d3b4a2
rizkidarmawan21 <rizkidarmawan.0402102@gmail.com>
2024-05-06 08:51:03 +0700

    add login page for customer page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,6 @@
-@include('pages.main.detail-product')
+{{-- @include('pages.main.login') --}}
+@include('pages.main.register')
+{{-- @include('pages.main.detail-product') --}}
 {{-- @include('pages.main.home') --}}
 {{-- @include('pages.main.cart') --}}
 {{-- @include('pages.main.checkout') --}}
